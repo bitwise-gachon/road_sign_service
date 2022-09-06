ddeae32aa7eeabde6e1204973dc6ef0803cc3def
MinhyukWoo <minhyukwoo.dev@gmail.com>
2022-09-06 13:35:05 +0900

    Add image delete feature and update css

--- src/components/view/ImagesView.jsx
+++ src/components/view/ImagesView.jsx
@@ -3,7 +3,8 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
 const Wrapper = styled.div`
-  margin: 0.5rem;
+  width: 100%;
+  margin: 0.5rem 0;
 `;
 
 const ImagesContainer = styled.div`
@@ -14,6 +15,7 @@ const ImagesContainer = styled.div`
 `;
 
 const ImageContainer = styled.div`
+  position: relative;
   height: 12rem;
   border: 0.0625rem solid grey;
   border-radius: 0.5rem;
@@ -26,14 +28,29 @@ const Image = styled.img`
   object-fit: cover;
 `;
 
-function ImagesView({ imageUrls }) {
+const DeleteButton = styled.div`
+  position: absolute;
+  top: 0.5rem;
+  right: 0.5rem;
+  border-radius: 1rem;
+  background-color: whitesmoke;
+  width: 1.25rem;
+  height: 1.25rem;
+`;
+
+function ImagesView({ imageUrls, onImageUrlDelete }) {
   return (
     <Wrapper>
       업로드한 이미지들
       <ImagesContainer>
-        {imageUrls.map((imageUrl, index) => (
-          <ImageContainer>
-            <Image src={imageUrl} alt={`${index}`} />
+        {imageUrls.map((imageUrl) => (
+          <ImageContainer key={imageUrl.key}>
+            <Image src={imageUrl.value} alt={`${imageUrl.key} image`} />
+            <DeleteButton
+              onClick={() => {
+                onImageUrlDelete(imageUrl.key);
+              }}
+            />
           </ImageContainer>
         ))}
       </ImagesContainer>
@@ -41,7 +58,10 @@ function ImagesView({ imageUrls }) {
   );
 }
 ImagesView.propTypes = {
-  imageUrls: PropTypes.string.isRequired,
+  imageUrls: PropTypes.arrayOf(
+    PropTypes.shape({ key: PropTypes.number, value: PropTypes.string }),
+  ).isRequired,
+  onImageUrlDelete: PropTypes.func.isRequired,
 };
 
 export default ImagesView;
--- src/pages/ImageUploadPage.jsx
+++ src/pages/ImageUploadPage.jsx
@@ -5,26 +5,48 @@ import ImagesView from '../components/view/ImagesView';
 
 const Wrapper = styled.div`
   border: 1px solid grey;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  padding: 1rem;
+`;
+
+const SubmitButton = styled.button`
+  right: 0rem;
+  height: 4rem;
+  width: 8rem;
 `;
 
 function ImageUploadPage() {
   const [imageUrls, setImageUrls] = useState([]);
+  const [imageUrlsCounter, setImageUrlsCounter] = useState(0);
   const onImageChange = (files) => {
-    Array.from(files).forEach((file) => {
-      const reader = new FileReader();
-      reader.onloadend = (event) => {
-        const { result } = event.target;
-        if (result) {
-          setImageUrls((state) => [...state, result]);
-        }
-      };
-      reader.readAsDataURL(file);
-    });
+    Array.from(files)
+      .filter((file) => file.type.match('image.*'))
+      .forEach((file, index) => {
+        const reader = new FileReader();
+        reader.onloadend = (event) => {
+          const { result } = event.target;
+          if (result) {
+            const imageUrl = { key: imageUrlsCounter + index, value: result };
+            setImageUrls((state) => [...state, imageUrl]);
+          }
+        };
+        reader.readAsDataURL(file);
+      });
+    setImageUrlsCounter((state) => state + files.length);
+  };
+  const onImageUrlDelete = (deleteKey) => {
+    const newImageUrls = imageUrls.filter(
+      (imageUrl) => imageUrl.key !== deleteKey,
+    );
+    setImageUrls(newImageUrls);
   };
   return (
     <Wrapper>
-      <ImageUploadBox onImageChange={onImageChange} />
-      <ImagesView imageUrls={imageUrls} />
+      <ImageUploadBox id="Upload Box" onImageChange={onImageChange} />
+      <ImagesView imageUrls={imageUrls} onImageUrlDelete={onImageUrlDelete} />
+      <SubmitButton type="submit">제출하기</SubmitButton>
     </Wrapper>
   );
 }
